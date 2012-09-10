5e836fc476d6871cf2bf8666772a6ef703339be1
danieljx <villanueva.daniel@me.com>
2012-09-10 01:17:26 -0430

    Cambio del puerto

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*var express = require('express'),http = require('http');
+var express = require('express'),http = require('http');
 var app = express();
 var server = http.createServer(app);
 app.set('views',__dirname + '/views');
@@ -8,25 +8,11 @@ app.configure(function(){
 app.get('/',function(req,res){
 	res.render('index.jade',{layout:false});
 });
-var port = process.env.C9_PORT || 8080 
-//20582 para nodester
-server.listen(port);*/
-
-var express = require('express');
-var app = express.createServer();
-app.set('views',__dirname + '/views');
-app.configure(function(){
-	app.use(express.static(__dirname));
-});
-app.get('/',function(req,res){
-	res.render('index.jade',{layout:false});
-});
 var port = process.env.C9_PORT || 20582 
 //20582 para nodester
-app.listen(port);
-
+server.listen(port);
 var io;
-io = require('socket.io').listen(app);
+io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket) {
 socket.on('drawClick', function(data) {
   socket.broadcast.emit('draw', {
