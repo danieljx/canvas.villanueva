aef0a8642d1967de297149028bc04b34d76e33d1
danieljx <villanueva.daniel@me.com>
2012-09-09 23:47:07 -0430

    Real Time Canvas html5, express, jade y socket.io

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express'),http = require('http');
+var app = express();
+var server = http.createServer(app);
+app.set('views',__dirname + '/views');
+app.configure(function(){
+	app.use(express.static(__dirname));
+});
+app.get('/',function(req,res){
+	res.render('index.jade',{layout:false});
+});
+var port = process.env.C9_PORT || 20582 //20582 para nodester
+server.listen(port);
+
+var io;
+io = require('socket.io').listen(server);
+io.sockets.on('connection', function(socket) {
+socket.on('drawClick', function(data) {
+  socket.broadcast.emit('draw', {
+	x: data.x,
+	y: data.y,
+	type: data.type
+  });
+});
+});
